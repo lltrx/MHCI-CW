2a3e374917506ef328b445d330fb4d0c64f79eb3
@2614579a <2614579a@student.gla.ac.uk>
2023-02-13 12:35:46 +0000

    added some pages and button that navigate between the two pages

--- Components/Pages/Destination.js
+++ Components/Pages/Destination.js
@@ -2,14 +2,14 @@ import { SafeAreaView, Text, View } from "react-native";
 import tailwind from "tailwind-rn";
 import NavigateButton from "../Utils/NavigateButton";
 
-export default function Home(props) {
+export default function Destination() {
   return (
     <SafeAreaView style={tailwind("flex-1 items-center justify-center")}>
       <View >
         <NavigateButton
-          style={tailwind("bg-blue-500 border-2 px-5 py-3 rounded-full")}
-          title="Navigate to Destination"
-          to="Destination"
+          style={tailwind("bg-blue-500 px-5 py-3 rounded-full")}
+          title="Navigate to Home"
+          to="Home"
         />
       </View>
     </SafeAreaView>
