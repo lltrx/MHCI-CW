135f92ea95e13d9b9ee56d58d22362de064d2e6c
@2614579a <2614579a@student.gla.ac.uk>
2023-02-27 12:53:28 +0000

    added a a new page for the map

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/Pages/Home';
 import Destination from './Components/Pages/Destination';
+import MapPage from './Components/Pages/MapPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Destination" component={Destination} />
+        <Stack.Screen name="MapPage" component={MapPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
