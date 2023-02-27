7f352ef5d761abafd0891c818d9f0323f0ae79c5
@2614579a <2614579a@student.gla.ac.uk>
2023-02-27 15:19:36 +0000

    Fixed the merge conflict on the dev branch

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './Components/Pages/Home';
-import Destination from './Components/Pages/Destination';
-import MapPage from './Components/Pages/MapPage';
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, Text, View, Button, StyleSheet } from "react-native";
+import tailwind from "tailwind-rn";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Components/Pages/Home";
+import Destination from "./Components/Pages/Destination";
+import Friends from "./Components/Pages/Friends";
+import FriendHistory from "./Components/Utils/FriendHistory";
+import MyProfile from "./Components/Pages/MyProfile";
+import MapPage from "./Components/Pages/MapPage";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Destination" component={Destination} />
+        <Stack.Screen name="Friends" component={Friends} />
+        <Stack.Screen name="FriendHistory" component={FriendHistory} />
+        <Stack.Screen name="MyProfile" component={MyProfile} />
         <Stack.Screen name="MapPage" component={MapPage} />
       </Stack.Navigator>
     </NavigationContainer>
