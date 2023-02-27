aed99a1248fe7061408d7d656eb2d9f928a82b8f
Faris bin musallam <2615938b@student.gla.ac.uk>
2023-02-27 15:48:32 +0300

    adding popup

--- App.js
+++ App.js
@@ -1,14 +1,79 @@
-import { SafeAreaView, Text, View } from "react-native";
-import tailwind from "tailwind-rn";
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View, Button, StyleSheet } from 'react-native';
+import tailwind from 'tailwind-rn';
+import Modal from 'react-native-modal';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 export default function App() {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
-    <SafeAreaView style={tailwind("flex-1 items-center justify-center")}>
-      <View style={tailwind("bg-blue-500 px-5 py-3 rounded-full")}>
-        <Text style={tailwind("text-white font-semibold text-lg")}>
-          We are ready to go!
-        </Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Button title="Open Checklist" onPress={toggleModal} />
+      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal} style={styles.modal}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Safety Checklist</Text>
+          <SwipeListView
+            data={['Option 1', 'Option 2', 'Option 3', 'Option 4']}
+            renderItem={({ item }) => (
+              <View style={styles.checklistItem}>
+                <Text style={styles.checklistItemText}>{item}</Text>
+              </View>
+            )}
+            renderHiddenItem={() => (
+              <View style={styles.rowBack}>
+                <Text style={styles.rowBackText}>Complete</Text>
+              </View>
+            )}
+            rightOpenValue={-75}
+          />
+        </View>
+      </Modal>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: '90%',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  checklistItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  checklistItemText: {
+    fontSize: 18,
+  },
+
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#ddd',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingLeft: 15,
+  },
+
+});
